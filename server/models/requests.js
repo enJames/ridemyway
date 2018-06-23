e5fd37b184bb17b4e17992885b5598c9fd690cbd
Enejo James <spec.en.james@gmail.com>
2018-06-23 20:45:58 +0100

    Add accepted property to requests model

--- server/models/requests.js
+++ server/models/requests.js
@@ -2,10 +2,12 @@ const requests = [
     {
         rideId: 1,
         userId: 2,
+        accepted: false
     },
     {
         rideId: 2,
         userId: 1,
+        accepted: true
     }
 ];
 
