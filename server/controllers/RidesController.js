27b4e95f68bb6a71fb6b8e6a586eb03965692ee3
Enejo James <spec.en.james@gmail.com>
2018-06-23 13:01:57 +0100

    Create ride offer controller function

--- server/controllers/RidesController.js
+++ server/controllers/RidesController.js
@@ -25,6 +25,32 @@ const RidesController = {
         }
 
         return SendResponse(res, 200, 'Ride found', theRide);
+    },
+    create: (req, res) => {
+        const {
+            from, to, price, seatsShared, seatsAvailable, driver
+        } = req.body;
+
+        // validate
+        if (!from || !to || !price || !seatsShared || !seatsAvailable || !driver) {
+            return SendResponse(res, 405, 'Please fillout all fields');
+        }
+
+        // Ride offer to create
+        const rideOffer = {
+            id: Rides.length + 1,
+            from,
+            to,
+            price,
+            seatsShared,
+            seatsAvailable,
+            driver
+        };
+
+        // Create ride offer
+        Rides.push(rideOffer);
+
+        return SendResponse(res, 201, 'Ride offer created', rideOffer);
     }
 };
 
