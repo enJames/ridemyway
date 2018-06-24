b20e633625987d08e79c55414497177d023b363b
Enejo James <spec.en.james@gmail.com>
2018-06-24 16:12:30 +0100

    Modify users dummy data model

--- server/models/users.js
+++ server/models/users.js
@@ -3,12 +3,16 @@ const users = [
         id: 1,
         firstname: 'Ajaniki',
         lastname: 'AirBnB',
+        email: 'ajan@airbnb.com',
+        password: 'pass',
         state: 'Lagos'
     },
     {
         id: 2,
         firstname: 'John',
         lastname: 'Dumelo',
+        email: 'john@dumelo.com',
+        password: 'word',
         state: 'Rivers'
     }
 ];
