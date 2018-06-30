6f88243e03111a8e553be9eb08e8cee7b9b5e441
Enejo James <spec.en.james@gmail.com>
2018-06-30 18:44:46 +0100

    Refactor sendResponse function

--- server/Reusables.js
+++ server/Reusables.js
@@ -1,17 +1,17 @@
 const Reusables = {
-    sendResponse: (
-        response,
-        statusCode,
-        message,
-        responseObject = null
-    ) => response.status(statusCode).json({
-        message,
-        responseObject
-    }),
-    sendErrors: (collatedErrors, response, next) => {
+    sendResponse: (response, statusCode, status, data) => {
+        response.status(statusCode).json({
+            status,
+            data
+        });
+    },
+    sendErrors: (response, collatedErrors, status, next) => {
         if (collatedErrors) {
             const errors = collatedErrors.map(eachError => eachError.msg);
-            return response.status(405).json({ errors });
+            return response.status(400).json({
+                status: 'fail',
+                errors
+            });
         }
         return next();
     }
