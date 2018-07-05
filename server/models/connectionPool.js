bbb91ae1ca69f6ab5ab59ef37055e4f74ac0f534
Enejo James <spec.en.james@gmail.com>
2018-07-05 15:10:39 +0100

    chore(documentation): recreate validation middlewares
    - install swagger-ui-express
    
    - add swagger json files
    
    q
    Gq
    :q
    eixt
    exit
    quit
    stop
    enter

--- server/models/connectionPool.js
+++ server/models/connectionPool.js
@@ -4,14 +4,17 @@ import { Pool } from 'pg';
 dotenv.config();
 
 let connectionString;
+let ssl;
 
 // set database for environment
 if (process.env.NODE_ENV === 'test') {
     connectionString = process.env.TEST_DATABASE_URL;
+    ssl = true;
 } else {
     connectionString = process.env.DATABASE_URL;
+    ssl = false;
 }
 
-const connectionPool = new Pool({ connectionString, ssl: true });
+const connectionPool = new Pool({ connectionString, ssl });
 
 export default connectionPool;
