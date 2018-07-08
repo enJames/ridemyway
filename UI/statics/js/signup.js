5bf501d8d0b53dd104c80870d2fdd6a37d93a378
Enejo James <spec.en.james@gmail.com>
2018-07-09 00:45:21 +0100

    chore(signupPage) user can signup from frontend
    - add paragraph element for displaying messages to the user
    
    - add property functions to PageFunctions object for controlling
      messages and element display.
    
    - elements displaying messages or show loading status stay invisible
      until triggered by the user then go invisible again
    
    - modify database table creation to allow null for for fields not on the
      signup page
    
     [Finishes #158873246]

--- UI/statics/js/signup.js
+++ UI/statics/js/signup.js
@@ -1,31 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const url = 'http://enjames-ridemyway.herokuapp.com/api/v1/auth/signup';
 
-    const firstname = document.getElementById('firstname');
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const confirmPassword = document.getElementById('confirmPassword');
-    const submit = document.getElementById('submit');
-
-    const userData = new FormData({
-        firstname,
-        email,
-        password,
-        confirmPassword
-    })
-
-    const fetchDataObject = {
-        method: 'POST',
-        body: userData,
-        headers: new Headers()
-    }
+    const url = 'https://enjames-ridemyway.herokuapp.com/api/v1/auth/signup';
 
     submit.addEventListener('click', (e) => {
         e.preventDefault();
 
+        const firstname = document.getElementById('firstname').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirmPassword').value;
+        const submit = document.getElementById('submit');
+
+        // message and spinner
+        const messageDiv = document.getElementById('message');
+        const spinner = document.getElementById('spinner');
+
+        // Display spinner while systems sends request
+        spinner.style.opacity = '1';
+
+        const userData = {
+            firstname,
+            email,
+            password,
+            confirmPassword
+        };
+
+        const fetchDataObject = {
+            method: 'POST',
+            body: JSON.stringify(userData),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+
         fetch(url, fetchDataObject)
-            .then(res => res)
-            .then(res => console.log(res))
-            .catch(error => console.error('There was an error', error));
+            .then(res => res.json())
+            .then((res) => {
+                console.log(res);
+                if (res.status === 'success') {
+                    return PageFunctions.showMessage(messageDiv, spinner, 'sucess', 'Sign up success!');
+                }
+                if (res.status === 'fail') {
+                    return PageFunctions.showMessage(messageDiv, spinner, 'fail', res.message);
+                }
+                if (res.status === 'error') {
+                    return PageFunctions.showMessage(messageDiv, spinner, 'fail', 'Email already exists');
+                }
+            })
+            .catch(error => PageFunctions.showMessage(messageDiv, spinner, 'error', 'It seems you are offline.'));
     }, false);
 }, false);
