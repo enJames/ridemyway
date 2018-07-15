f468b77a5e628d7edfa3fe280a5c11a07e053e2c
Enejo James <spec.en.james@gmail.com>
2018-07-15 05:56:09 +0100

    chore(login): implement login from front-end
    - make a get request to check route on server to check user logged in status
    
    - allow user login using login form if user is not logged in
    
    - redirect user to dashboard if logged in
    
    - add script tag to link login javascript file to login page
    
    [Finishes #159057153]

---
+++ UI/statics/js/login.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://enjames-ridemyway.herokuapp.com/api/v1/auth/check', {
+        method: 'GET',
+        credentials: 'include'
+    })
+        .then(res => res.json())
+        .then((res) => {
+            // if user is logged in
+            if (res.status === 'fail') {
+                return location.replace('https://enjames.github.io/ridemyway/UI/dashboard.html');
+            }
+
+            // if user is not logged in
+            const url = 'https://enjames-ridemyway.herokuapp.com/api/v1/auth/login';
+
+            submit.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                const email = document.getElementById('email').value;
+                const password = document.getElementById('password').value;
+                const submit = document.getElementById('submit');
+
+                // message and spinner
+                const messageEl = document.getElementById('message');
+                const spinner = document.getElementById('spinner');
+
+                // Display spinner while systems sends request
+                spinner.style.opacity = '1';
+
+                const userData = {
+                    email,
+                    password
+                };
+
+                const fetchDataObject = {
+                    method: 'POST',
+                    body: JSON.stringify(userData),
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    credentials: 'include'
+                }
+
+                fetch(url, fetchDataObject)
+                    .then(res => res.json())
+                    .then((res) => {
+                        console.log(res);
+                        return PageFunctions.showMessage(res.status, res.message);
+                    })
+                    .catch(error => PageFunctions.showMessage(res.status, res.message));
+            }, false);
+        })
+        .catch((err) => console.error('There was a problem', err));
+}, false);
