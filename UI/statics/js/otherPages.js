843947086b80c8bfdf4c9c813b83db02262cd57c
Enejo James <spec.en.james@gmail.com>
2018-07-18 01:58:32 +0100

    fix element null

--- UI/statics/js/otherPages.js
+++ UI/statics/js/otherPages.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navigationToggler = document.getElementById('navigationToggler');
-    const navigationDiv = document.getElementById('navigationId');
-    const navigationItemsWrapper = document.getElementsByClassName('navigation-items-wrapper')[0];
-    const userNavToggler = document.getElementById('userNavToggler');
-    const userNav = document.getElementById('userNav');
+
 
 
     // Toggle collapse
     navigationToggler.addEventListener('click', () => {
+        const navigationDiv = document.getElementById('navigationId');
+        const navigationItemsWrapper = document.getElementsByClassName('navigation-items-wrapper')[0];
+        const userNavToggler = document.getElementById('userNavToggler');
+
         PageFunctions
             .collapseToggle(
                 navigationToggler,
@@ -18,6 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Toggle User nav display
     document.addEventListener('click', (e) => {
+        const userNav = document.getElementById('userNav');
+        
         PageFunctions.displayToggle(e, userNav);
     }, false);
 
