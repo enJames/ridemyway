945c250f616b7bde3b1acc94c457fd9cb3be00a8
Enejo James <spec.en.james@gmail.com>
2018-07-21 10:37:53 +0100

    chore(dashboard): implement fetch dashboard on front-end
    change css structure to aid proper font styling
    
    add general.css to ensure loading font maintain personalised font-size for each page
    
    add see details button to dashboard elements
    
    adjust margin and padding of dashboard container
    
    remove redundant navigation element in user-navigation
    
    fetch dashboard data from api using fetch
    
    add helper functions to PageFunctions object to aid date and time conversation to user-friendly formats
    
    display fetch dashboard
    
    redirect unauthenticated user to login page
    
    [Finishes #159059493]

--- UI/statics/js/dashboard.js
+++ UI/statics/js/dashboard.js
@@ -1,16 +1,69 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://enjames-ridemyway.herokuapp.com/api/v1/auth/check', {
+    fetch('https://enjames-ridemyway.herokuapp.com/api/v1/users/dashboard', {
         method: 'GET',
         credentials: 'include'
     })
         .then(res => res.json())
         .then((res) => {
             // if user is logged in
-            if (res.status === 'fail') {
+            if (res.status === 'fail' && res.message === 'Not authenticated') {
                 return location.replace('https://enjames.github.io/ridemyway/UI/login.html?auth=false');
             }
 
-            enableLogout();
+            // activate logout functionality
+            PageFunctions.enableLogout();
+
+            if (res.status === 'success') {
+                const { runningOffer, runningJoinRequest } = res.data;
+                const rideSummary = document.getElementById('rideSummary');
+                const rideBooking = document.getElementById('rideBooking');
+
+                const departureDate = PageFunctions.dateToWords(runningOffer.departureDate);
+                const departureTime = PageFunctions.to12to12hrFormat(runningOffer.departureTime);
+
+                let runningOfferHTML = '';
+                let runningJoinRequestHTML = '';
+
+                // Check if an object is returned
+                if (typeof(runningOffer) === 'object') {
+                    runningOfferHTML = `<div class="trip">
+                        <span>${runningOffer.fromState}</span>
+                        <i class="fa fa-long-arrow-right"></i>
+                        <span>${runningOffer.toState}</span>
+                    </div>
+                    <div class="departure">
+                        <p>Departure Date: <span>${PageFunctions.dateToWords(runningOffer.departureDate)}</span></p>
+                        <p>Departure Time: <span>${PageFunctions.dateToWords(runningOffer.departureTime)}</span></p>
+                    </div>
+                    <div class="btn-container">
+                        <a href="responses.html">See details</a>
+                    </div>`
+                } else {
+                    runningOfferHTML = runningOffer;
+                }
+
+                if (typeof(runningJoinRequest) === 'object') {
+                    runningJoinRequestHTML = `<div class="trip">
+                        <span>${runningJoinRequest.fromState}</span>
+                        <i class="fa fa-long-arrow-right"></i>
+                        <span>${runningJoinRequest.toState}</span>
+                    </div>
+                    <div class="departure">
+                        <p>Departure Date: <span>${PageFunctions.dateToWords(runningJoinRequest.departureDate)}</span></p>
+                        <p>Departure Time: <span>${PageFunctions.dateToWords(runningJoinRequest.departureTime)}</span></p>
+                    </div>
+                    <div class="btn-container">
+                        <a href="responses.html">See details</a>
+                    </div>`
+                } else {
+                    runningJoinRequestHTML = runningJoinRequest;
+                }
+
+                // Append to DOM
+                rideSummary.innerHTML = runningOfferHTML;
+                rideBooking.innerHTML = runningJoinRequestHTML;
+
+                return { rideSummary.innerHTML, rideBooking.innerHTML };
         })
         .catch((err) => console.error('There was a problem', err));
 }, false);
